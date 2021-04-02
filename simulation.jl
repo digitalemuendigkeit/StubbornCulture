e96514666ffc9c3f5ff8046eb551130f3f9d9828
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2021-04-03 00:17:37 +0200

    model works, second experiment might be ready too

--- simulation.jl
+++ simulation.jl
@@ -13,16 +13,17 @@ mutable struct AxelrodAgent <: Agents.AbstractAgent
     changed_culture::Bool
 end
 
-function init_random_agent(id, pos)
-    culture = rand(0:9, 5)
+function init_random_agent(id, pos, model::Agents.AgentBasedModel)
+    culture = rand(0:(model.trait_dims - 1), model.culture_dims)
     return AxelrodAgent(id, pos, false, culture, false)
 end
 
 function to_stubborn!(positions::Array{Int}, model::Agents.AgentBasedModel, value::Int64=0)
+    dims = model.culture_dims
     for pos in positions
         stubborn_agent = model.agents[pos]
         stubborn_agent.stubborn = true
-        stubborn_agent.culture = fill(value, 5)
+        stubborn_agent.culture = fill(value, dims)
     end
     return model
 end
@@ -56,16 +57,76 @@ end
 
 function populate!(model::Agents.AgentBasedModel)
     for i in 1:nv(model.space.graph)
-        add_agent!(init_random_agent(i, i), i, model)
+        add_agent!(init_random_agent(i, i, model), i, model)
     end
     return model
 end
 
+function create_model(space::Agents.GraphSpace, culture_dims::Int, trait_dims::Int, stubborn_positions::Union{Dict, Array{Int}}) 
+    properties = Dict(
+        :culture_dims => culture_dims,
+        :trait_dims => trait_dims
+    )
+    model = Agents.AgentBasedModel(AxelrodAgent, space, properties = properties)
+    populate!(model)
+    to_stubborn!(stubborn_positions, model)
+    return model
+end
+
+
+
+# CONVERGENCE WITH TWO CULTURAL DIMENSIONS
 space = Agents.GraphSpace(LightGraphs.smallgraph(:karate))
-model = Agents.AgentBasedModel(AxelrodAgent, space)
-populate!(model)
-to_stubborn!([1], model)
-adata, _ = run!(model, agent_step!, 500, adata = [:stubborn, :culture, :changed_culture], replicates = 3, obtainer = copy)
-adata[!, "culture"] = [join(c) for c in adata[!, "culture"]]
-Arrow.write(joinpath("data", "data.arrow"), adata)
-# DataFrame(Arrow.Table("data"))
+model1 = create_model(space, 2, 2, Dict(0 => [34], 1 => [1]))
+adata1, _ = run!(model1, agent_step!, 500, adata = [:stubborn, :culture, :changed_culture], replicates = 100, obtainer = copy)
+adata1[!, "culture"] = [join(c) for c in adata1[!, "culture"]]
+Arrow.write(joinpath("data", "convergence1.arrow"), adata1)
+
+# CONVERGENCE WITH THREE CULTURAL DIMENSIONS
+space = Agents.GraphSpace(LightGraphs.smallgraph(:karate))
+model2 = create_model(space, 3, 2, Dict(0 => [34], 1 => [1]))
+adata2, _ = run!(model2, agent_step!, 500, adata = [:stubborn, :culture, :changed_culture], replicates = 100, obtainer = copy)
+adata2[!, "culture"] = [join(c) for c in adata2[!, "culture"]]
+Arrow.write(joinpath("data", "convergence2.arrow"), adata2)
+
+# CONVERGENCE WITH FOUR CULTURAL DIMENSIONS
+space = Agents.GraphSpace(LightGraphs.smallgraph(:karate))
+model3 = create_model(space, 4, 2, Dict(0 => [34], 1 => [1]))
+adata3, _ = run!(model3, agent_step!, 500, adata = [:stubborn, :culture, :changed_culture], replicates = 100, obtainer = copy)
+adata3[!, "culture"] = [join(c) for c in adata3[!, "culture"]]
+Arrow.write(joinpath("data", "convergence3.arrow"), adata3)
+
+# CONVERGENCE WITH FIVE CULTURAL DIMENSIONS
+space = Agents.GraphSpace(LightGraphs.smallgraph(:karate))
+model4 = create_model(space, 5, 2, Dict(0 => [34], 1 => [1]))
+adata4, _ = run!(model4, agent_step!, 500, adata = [:stubborn, :culture, :changed_culture], replicates = 100, obtainer = copy)
+adata4[!, "culture"] = [join(c) for c in adata4[!, "culture"]]
+Arrow.write(joinpath("data", "convergence4.arrow"), adata4)
+
+
+
+# NEXT STEPS
+
+# use the model with 3 cultural dimensions
+# choose two agents at random and simulate the outcomes repeatedly
+# compute the probability for a schism
+
+space = Agents.GraphSpace(LightGraphs.smallgraph(:karate))
+df_list = DataFrame[]
+for i in 1:33
+    for j in (i + 1):34
+        model = create_model(space, 3, 2, Dict(0 => [i], 1 => [j]))
+        adata, _ = run!(
+            model, agent_step!, 500, 
+            adata = [:stubborn, :culture, :changed_culture], 
+            replicates = 100, obtainer = copy, when = [500]
+        )
+        adata[!, "culture"] = [join(c) for c in adata[!, "culture"]]
+        adata[!, "leader1"] .= i
+        adata[!, "leader2"] .= j
+        select!(adata, Not(:step))
+        push!(df_list, deepcopy(adata))
+    end
+end
+all_combs = vcat(df_list..., cols = :union)
+Arrow.write(joinpath("data", "size_test.arrow"), adata)
